d8d60cb149c8111eafaf549b6437878d01ee5500
Adrian Oprea <adrian@codesi.nz>
2016-10-17 17:33:47 +0300

    feat: Enable main nav toggle with JS

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,8 @@
-// This is where it all goes :)
+'use strict';
+
+const menuButtonToggle = document.querySelector('#mainMenuToggle');
+const mainMenu = document.querySelector('#mainMenu');
+
+menuButtonToggle.addEventListener('click', () => (
+  mainMenu.classList.toggle('c-main-menu-items__visible')
+), false);
