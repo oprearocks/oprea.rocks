cc39f13394876006fc7be7ed9232e6835d5e9ee7
Adrian Oprea <adrian@codesi.nz>
2016-10-18 18:30:28 +0300

    feat: Test for touch events and menu swap position

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,3 +6,25 @@ const mainMenu = document.querySelector('#mainMenu');
 menuButtonToggle.addEventListener('click', () => (
   mainMenu.classList.toggle('c-main-menu-items__visible')
 ), false);
+
+// let lCounter = 0;
+// let rCounter = 0;
+// let times = 0;
+// let viewportWidth = document.body.offsetWidth;
+
+// document.addEventListener('touchstart', (e) => {
+//   times++;
+
+//   if (e.touches[0].pageX < viewportWidth / 2) {
+//     lCounter++;
+//   } else {
+//     rCounter++;
+//   }
+
+//   if (times > 3) {
+//     let cond = prompt("Are you left handed?");
+//     if (cond) {
+//       document.querySelector('.c-header__spaced').classList.add('u-swap-order');
+//     }
+//   }
+// }, false);
