54787142c215c52d664ba3ac24ce900dbe0947e4
Adrian Oprea <adrian@codesi.nz>
2016-11-16 19:26:30 +0200

    feat: Add blogging module configuration
    - Configure permalinks
    - Configure sources
    
    Signed-off-by: Adrian Oprea <adrian@codesi.nz>

--- config.rb
+++ config.rb
@@ -27,7 +27,28 @@ activate :directory_indexes
 activate :autoprefixer
 
 activate :blog do |blog|
-  blog.prefix = "blog"
+    # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  blog.sources = "posts/{year}-{month}-{day}-{title}.html"
+  blog.permalink = "blog/{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.taglink = "blog/tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  blog.year_link = "blog/{year}.html"
+  blog.month_link = "blog/{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".md"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
 end
 
 ###
