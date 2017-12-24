0d8271645ab1cade685b8e70de7673cea8aae6cc
Adrian Oprea <adrian@oprea.rocks>
2017-12-24 10:23:09 +0200

    chore: Update newsletter text
    Signed-off-by: Adrian Oprea <adrian@oprea.rocks>

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -8,7 +8,7 @@ const Newsletter = () => (
   <section className="sidebar-section about">
     <h2 data-drip-attribute="headline" className="section-title separator-below">Newsletter</h2>
     <section className="section-content">
-      <p data-drip-attribute="description">Join my newsletter to get the latest things I'm reading or learning about, directly to your inbox.</p>
+      <p data-drip-attribute="description">Join my newsletter to get the latest updates on what I'm reading or learning about, straight to your inbox.</p>
       <form data-drip-embedded-form="92273073" method="post" action="https://www.getdrip.com/forms/92273073/submissions">
         <div>
           <input type="email" name="fields[email]" placeholder="email@example.com" className="form-field full-width" type="email" />
