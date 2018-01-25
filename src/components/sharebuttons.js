f4f44e6b3bf7cc2f1c62fccc3345167cb876863f
Adrian Oprea <adrian@oprea.rocks>
2018-01-25 07:23:52 +0200

    chore: Disable unnecessary share buttons

--- src/components/sharebuttons.js
+++ src/components/sharebuttons.js
@@ -6,39 +6,39 @@ import {
 } from 'react-share'
 
 const {
-  FacebookShareButton,
-  GooglePlusShareButton,
-  LinkedinShareButton,
   TwitterShareButton,
-  WhatsappShareButton,
-  RedditShareButton,
-  EmailShareButton,
+  FacebookShareButton,
+  LinkedinShareButton,
+  // GooglePlusShareButton,
+  // WhatsappShareButton,
+  // RedditShareButton,
+  // EmailShareButton,
 } = ShareButtons;
 
-const FacebookIcon = generateShareIcon('facebook');
 const TwitterIcon = generateShareIcon('twitter');
-const GooglePlusIcon = generateShareIcon('google');
+const FacebookIcon = generateShareIcon('facebook');
 const LinkedinIcon = generateShareIcon('linkedin');
-const WhatsappIcon = generateShareIcon('whatsapp');
-const RedditIcon = generateShareIcon('reddit');
-const EmailIcon = generateShareIcon('email');
+// const GooglePlusIcon = generateShareIcon('google');
+// const WhatsappIcon = generateShareIcon('whatsapp');
+// const RedditIcon = generateShareIcon('reddit');
+// const EmailIcon = generateShareIcon('email');
 
 import './sharebuttons.scss'
 
 const SocialButtons = ({ url, description = '', title }) => (
   <div className="share-buttons">
-    <FacebookShareButton url={url}>
-      <FacebookIcon size={32} round/>
-    </FacebookShareButton>
-    <GooglePlusShareButton url={url}>
-      <GooglePlusIcon size={32} round/>
-    </GooglePlusShareButton>
-    <LinkedinShareButton url={url} description={description} title={title}>
-      <LinkedinIcon size={32} round/>
-    </LinkedinShareButton>
     <TwitterShareButton url={url} via="oprearocks" title={title}>
       <TwitterIcon size={32} round/>
     </TwitterShareButton>
+    <FacebookShareButton url={url}>
+      <FacebookIcon size={32} round/>
+    </FacebookShareButton>
+    <LinkedinShareButton url={url} description={description} title={title}>
+      <LinkedinIcon size={32} round/>
+    </LinkedinShareButton>
+    {/* <GooglePlusShareButton url={url}>
+      <GooglePlusIcon size={32} round/>
+    </GooglePlusShareButton>
     <WhatsappShareButton url={url} title={title} separator=" / ">
       <WhatsappIcon size={32} round/>
     </WhatsappShareButton>
@@ -47,7 +47,7 @@ const SocialButtons = ({ url, description = '', title }) => (
     </RedditShareButton>
     <EmailShareButton url={url} subject={title} body={url}>
       <EmailIcon size={32} round/>
-    </EmailShareButton>
+    </EmailShareButton> */}
   </div>
 )
 
