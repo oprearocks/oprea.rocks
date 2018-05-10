e26baacc763206db8218123701a2d0135d12d8de
Adrian Oprea <adrian@oprea.rocks>
2018-05-10 23:32:56 +0300

    chore: Reddit still deserves to be there
    Signed-off-by: Adrian Oprea <adrian@oprea.rocks>

--- src/components/sharebuttons.js
+++ src/components/sharebuttons.js
@@ -11,7 +11,7 @@ const {
   LinkedinShareButton,
   // GooglePlusShareButton,
   // WhatsappShareButton,
-  // RedditShareButton,
+  RedditShareButton,
   // EmailShareButton,
 } = ShareButtons;
 
@@ -20,31 +20,31 @@ const FacebookIcon = generateShareIcon('facebook');
 const LinkedinIcon = generateShareIcon('linkedin');
 // const GooglePlusIcon = generateShareIcon('google');
 // const WhatsappIcon = generateShareIcon('whatsapp');
-// const RedditIcon = generateShareIcon('reddit');
+const RedditIcon = generateShareIcon('reddit');
 // const EmailIcon = generateShareIcon('email');
 
 import './sharebuttons.scss'
 
 const SocialButtons = ({ url, description = '', title }) => (
   <div className="share-buttons">
-    <TwitterShareButton url={url} via="oprearocks" title={title}>
-      <TwitterIcon size={32} round/>
-    </TwitterShareButton>
-    <FacebookShareButton url={url}>
-      <FacebookIcon size={32} round/>
-    </FacebookShareButton>
     <LinkedinShareButton url={url} description={description} title={title}>
       <LinkedinIcon size={32} round/>
     </LinkedinShareButton>
+    <TwitterShareButton url={url} via="oprearocks" title={title}>
+      <TwitterIcon size={32} round/>
+    </TwitterShareButton>
+    <RedditShareButton url={url} title={title}>
+      <RedditIcon size={32} round/>
+    </RedditShareButton>
+    <FacebookShareButton url={url}>
+      <FacebookIcon size={32} round/>
+    </FacebookShareButton>
     {/* <GooglePlusShareButton url={url}>
       <GooglePlusIcon size={32} round/>
     </GooglePlusShareButton>
     <WhatsappShareButton url={url} title={title} separator=" / ">
       <WhatsappIcon size={32} round/>
     </WhatsappShareButton>
-    <RedditShareButton url={url} title={title}>
-      <RedditIcon size={32} round/>
-    </RedditShareButton>
     <EmailShareButton url={url} subject={title} body={url}>
       <EmailIcon size={32} round/>
     </EmailShareButton> */}
