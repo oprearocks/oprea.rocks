2c08ea150e5975252933ae473fb508f18111e802
Adrian Oprea <adrian@oprea.rocks>
2018-06-23 16:31:04 +0300

    Fix image path for facebook OG description

--- src/components/postMeta.js
+++ src/components/postMeta.js
@@ -17,7 +17,7 @@ const PostMeta = ({ post }) => (
       { property: 'og:type', content: 'article' },
       { property: 'og:title', content: post.title },
       { property: 'og:description', content: post.description || post.content.childMarkdownRemark.excerpt },
-      { property: 'og:image', content: post.postImage && `https:${post.postImage.resolutions.src}` || logo},
+      { property: 'og:image', content: post.postImage && `https:${post.postImage.resolutions.src}` || `https://oprea.rocks/${logo}`},
       { property: 'og:url', content: `https://oprea.rocks/blog/${post.permalink}` },
       { property: 'og:locale', content: 'en_US' },
 
