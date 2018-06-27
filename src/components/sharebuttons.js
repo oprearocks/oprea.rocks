afcbaa0aa4d493483995b0cee2d34ce78d1bcf94
Adrian Oprea <adrian@oprea.rocks>
2018-06-27 23:44:15 +0300

    refactor: Update to react-share 2.0

--- src/components/sharebuttons.js
+++ src/components/sharebuttons.js
@@ -1,53 +1,44 @@
 import React from 'react'
 import {
-  ShareButtons,
-  ShareCounts,
-  generateShareIcon
-} from 'react-share'
-
-const {
-  TwitterShareButton,
   FacebookShareButton,
+  GooglePlusShareButton,
   LinkedinShareButton,
-  // GooglePlusShareButton,
-  // WhatsappShareButton,
+  TwitterShareButton,
   RedditShareButton,
-  // EmailShareButton,
-} = ShareButtons;
+  TumblrShareButton,
+} from 'react-share';
 
-const TwitterIcon = generateShareIcon('twitter');
-const FacebookIcon = generateShareIcon('facebook');
-const LinkedinIcon = generateShareIcon('linkedin');
-// const GooglePlusIcon = generateShareIcon('google');
-// const WhatsappIcon = generateShareIcon('whatsapp');
-const RedditIcon = generateShareIcon('reddit');
-// const EmailIcon = generateShareIcon('email');
+import {
+  FacebookIcon,
+  GooglePlusIcon,
+  LinkedinIcon,
+  TwitterIcon,
+  RedditIcon,
+  TumblrIcon,
+} from 'react-share';
 
 import './sharebuttons.scss'
 
-const SocialButtons = ({ url, description = '', title }) => (
+const SocialButtons = ({ url, description = '', title, tags = [] }) => (
   <div className="share-buttons">
-    <LinkedinShareButton url={url} description={description} title={title}>
-      <LinkedinIcon size={32} round/>
-    </LinkedinShareButton>
-    <TwitterShareButton url={url} via="oprearocks" title={title}>
-      <TwitterIcon size={32} round/>
-    </TwitterShareButton>
-    <RedditShareButton url={url} title={title}>
-      <RedditIcon size={32} round/>
-    </RedditShareButton>
     <FacebookShareButton url={url}>
-      <FacebookIcon size={32} round/>
+      <FacebookIcon size={40}/>
     </FacebookShareButton>
-    {/* <GooglePlusShareButton url={url}>
-      <GooglePlusIcon size={32} round/>
+    <TwitterShareButton url={url} via="oprearocks" title={title}>
+      <TwitterIcon size={40}/>
+    </TwitterShareButton>
+    <LinkedinShareButton url={url} description={description} title={title}>
+      <LinkedinIcon size={40}/>
+    </LinkedinShareButton>
+    <GooglePlusShareButton url={url} description={description} title={title}>
+      <GooglePlusIcon size={40}/>
     </GooglePlusShareButton>
-    <WhatsappShareButton url={url} title={title} separator=" / ">
-      <WhatsappIcon size={32} round/>
-    </WhatsappShareButton>
-    <EmailShareButton url={url} subject={title} body={url}>
-      <EmailIcon size={32} round/>
-    </EmailShareButton> */}
+    <RedditShareButton url={url} title={title}>
+      <RedditIcon size={40}/>
+    </RedditShareButton>
+    <TumblrShareButton url={url} title={title} caption={description} tags={tags}>
+      <TumblrIcon size={40}/>
+    </TumblrShareButton>
   </div>
 )
 
