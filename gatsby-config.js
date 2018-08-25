f9cccbe7e923ded9663b0eb51f04d2a7b5fe208f
Adrian Oprea <adrian@oprea.rocks>
2018-08-25 13:27:38 +0300

    featore: Enable gatsby-plugin-podcast-feed in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,40 @@ module.exports = {
           {
             resolve: `gatsby-remark-prismjs`,
           },
+          {
+            resolve: `gatsby-plugin-podcast-feed`,
+            options: {
+              title: 'Dev Time Stories',
+              description: 'Full Stack Software Developer Adrian Oprea shares his thoughts on software development, tooling, entrepreneurship and product development. From the nitty-gritty of JavaScript development, with or without frameworks, to DevOps, tooling and automation, all the way to entrepreneurship and product development, this podcast is focused specifically on software development professionals.',
+              feed_url: 'https://oprea.rocks/podcast.xml',
+              site_url: 'https://oprea.rocks',
+              image_url: 'http://s3.eu-west-2.amazonaws.com/dev-time-stories-podcast/podcast-artwork.png',
+              author: 'Adrian Oprea ',
+              managingEditor: 'Adrian Oprea',
+              webMaster: 'Adrian Oprea',
+              copyright: `${(new Date()).getFullYear()} Adrian Oprea`,
+              language: 'en',
+              categories: ['Software development', 'Entrepreneurship', 'Technology'],
+              ttl: '1',
+              itunesAuthor: 'Adrian Oprea',
+              itunesSubtitle: 'Adrian Oprea — Full Stack JavaScript Consultant',
+              itunesSummary: 'Full Stack Software Developer Adrian Oprea shares his thoughts on software development, tooling, entrepreneurship and product development. From the nitty-gritty of JavaScript development, with or without frameworks, to DevOps, tooling and automation, all the way to entrepreneurship and product development, this podcast is focused specifically on software development professionals.',
+              itunesOwner: { name: 'Adrian Oprea', email:'adrian@oprea.rocks' },
+              itunesExplicit: false,
+              itunesCategory: {
+                  'text': 'Technology',
+                  'subcats': [
+                    {
+                      'text': 'Tech News'
+                    },
+                    {
+                      'text': 'Software How-To'
+                    }
+                  ]
+              },
+              itunesImage: 'http://s3.eu-west-2.amazonaws.com/dev-time-stories-podcast/podcast-artwork.png'
+            },
+          }
         ],
       },
     },
